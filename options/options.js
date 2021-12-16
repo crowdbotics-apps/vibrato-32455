1676e9b71b78e658390565176d8cc70fb72ee881
Crowdbotics <team@crowdbotics.com>
2021-12-16 20:44:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vibrato_32455",
+  url: "https://vibrato_32455.botics.co",
+  api: "https://vibrato_32455.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
